2932db628eb2e33c9475ba41b2806b0fc628da18
Adrian Voicu <avenir.ro@gmail.com>
2015-02-05 16:28:20 +0200

    Why repeat yourself?
    ...

--- helpers/logger_helper.php
+++ helpers/logger_helper.php
@@ -15,45 +15,31 @@
     * @param $user,    string, the user doing the handle.
     * @param $message, string, the handling the user is doing.
     */
-   if (! function_exists('user_log')) {
-     function user_log($user, $message) {
-       // Set instance and load config.
-       $CI =& get_instance();
-       $CI->load->config('logger_config', TRUE);
+  if (! function_exists('user_log')) {
+    function user_log($user, $message) {
+      // Set instance and load config.
+      $CI =& get_instance();
+      $CI->load->config('logger_config', TRUE);
 
-       $Date = strtotime(date($CI->config->item('date_format')));
+      $Date = strtotime(date($CI->config->item('date_format')));
 
-       $Filepath =  './application/logs/log-'. $Date .'.php';
-
-       if(! file_exists($Filepath)) {
-         // File doesn't exists so we need to first write it.
-         $Fileheader = "<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>\n\n";
-         $LogMessage = '['. date($CI->config->item('time_format')). ']: '. $user .' --> '. $message ."\n";
-
-         // Open the log file
-         $Logfile = fopen($Filepath, "a");
-
-         // Write to the file.
-         fwrite($Logfile, $Fileheader);
-         fwrite($Logfile, $LogMessage);
-         // Close file
-         fclose($Logfile);
-
-         // Setting rights to the file
-         chmod($Filepath, $CI->config->item('chmod'));
-
-         return TRUE;
-       } else {
-         // The file exists sp we are write te log message only.
-         $LogMessage = '['. date($CI->config->item('time_format')). ']: '. $user .' --> '. $message ."\n";
-
-         // Open the log file
-         $Logfile = fopen($Filepath, "a");
-
-         // Write to the file.
-         fwrite($Logfile, $LogMessage);
-         // Close file
-         fclose($Logfile);
-       }
+      $Filepath =  './application/logs/log-'. $Date .'.php';
+     
+      $LogMessage = '['. date($CI->config->item('time_format')). ']: '. $user .' --> '. $message ."\n";
+      
+      if(! file_exists($Filepath)) {
+        // File doesn't exists so we need to first write it.
+        $LogMessage = "<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>\n\n".$LogMessage;
+      }
+      // Open the log file
+      $Logfile = fopen($Filepath, "a");
+      chmod($Filepath, $CI->config->item('chmod'));
+      // Write to the file.
+      if(fwrite($Logfile, $LogMessage))
+      {
+        fclose($Logfile)
+        return TRUE;
+      }
+      return FALSE;
     }
   }
